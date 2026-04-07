795368a04335c4be3d6b6121bff07dae64913d99
dangdang122 <vmfhxhtm122@naver.com>
2026-04-07 11:50:34 +0900

    5장 도서 상세정보, 카테고리별 도서 목록,도서 카테고리와 출판사가 일치하는 필터링 결과 도서 목록(Controller 미완성)

--- src/main/java/kr/ac/kopo/jong/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/jong/bookmarket/controller/BookController.java
@@ -5,8 +5,11 @@ import kr.ac.kopo.jong.bookmarket.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -20,5 +23,21 @@ public class BookController {
         List<Book> listOfBooks = bookService.getAllBookList();
         model.addAttribute("bookList", listOfBooks);
         return "books";
+    }
+
+    @GetMapping("/book")
+    public String requestBookById(@RequestParam("id") String bookId, Model model){
+        Book book = bookService.getBookById(bookId);
+        model.addAttribute("book",book);
+        return "book";
+    }
+
+    @GetMapping("all")
+    public ModelAndView requestAllBooks(){
+        ModelAndView modelAndView = new ModelAndView();
+        List<Book> list = bookService.getAllBookList();
+        modelAndView.addObject("bookList", list);
+        modelAndView.setViewName("books");
+        return modelAndView;
     }
 }
--- src/main/java/kr/ac/kopo/jong/bookmarket/service/BookService.java
+++ src/main/java/kr/ac/kopo/jong/bookmarket/service/BookService.java
@@ -2,7 +2,17 @@ package kr.ac.kopo.jong.bookmarket.service;
 
 import kr.ac.kopo.jong.bookmarket.domain.Book;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.springframework.stereotype.Service;
+@Service
 public interface BookService {
     List<Book> getAllBookList();
+
+    Book getBookById(String bookId);
+
+    List<Book> getBookListByCategory(String category);
+
+    Set<Book> getBookListByFilter(Map<String, List<String>> filter);
 }
